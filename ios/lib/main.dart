90ad0f8f9a1ceeb7188aea40dd67841b0011bb37
luizueir0 <luis.boa.rig@gmail.com>
2026-03-22 17:28:00 -0300

    Atividade 03 - Luis Eduardo Boaretto Rigoni - 5-B-N - RA: 2404089

--- ios/lib/main.dart
+++ ios/lib/main.dart
@@ -1,121 +1,269 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Category extends StatelessWidget {
+  final Widget child;
+  final EdgeInsetsGeometry margin;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const Category({
+    super.key,
+    required this.child,
+    this.margin = const EdgeInsets.only(top: 8),
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Container(margin: margin, alignment: Alignment.center, child: child);
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+
+  String _product = "";
+  String _deliveryType = "Regular";
+  bool _acceptEmail = false;
+  double _quantity = 0;
+  String? _selectedRegion;
+
+  String? _resultProduct;
+  double? _resultQuantity;
+  String? _resultDeliveryType;
+  String? _resultRegion;
+  bool? _resultAcceptEmail;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: AppBar(title: const Text('Olá Flutter')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Preenche com o nome do produto:',
+                style: TextStyle(color: Colors.black87),
+              ),
+              const SizedBox(height: 5),
+
+              TextField(
+                controller: _controller,
+                onChanged: (value) {
+                  setState(() {
+                    _product = value;
+                  });
+                },
+                decoration: const InputDecoration(
+                  suffixIcon: Icon(Icons.account_box_outlined),
+                ),
+              ),
+
+              const SizedBox(height: 5),
+
+              const Category(
+                child: Text(
+                  'Defina a quantidade:',
+                  style: TextStyle(color: Colors.black87),
+                ),
+              ),
+
+              Slider(
+                value: _quantity,
+                max: 100,
+                min: 0,
+                onChanged: (double value) {
+                  setState(() {
+                    _quantity = value;
+                  });
+                },
+              ),
+
+              const SizedBox(height: 5),
+
+              const Category(
+                child: Text(
+                  'Escolha o tipo de entrega:',
+                  style: TextStyle(color: Colors.black87),
+                ),
+              ),
+
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Radio<String>(
+                        value: 'Carreto',
+                        groupValue: _deliveryType,
+                        onChanged: (v) => setState(() => _deliveryType = v!),
+                      ),
+
+                      const Text("Carreto"),
+                    ],
+                  ),
+
+                  Row(
+                    children: [
+                      Radio<String>(
+                        value: 'Retirada',
+                        groupValue: _deliveryType,
+                        onChanged: (v) => setState(() => _deliveryType = v!),
+                      ),
+
+                      const Text("Retirada"),
+                    ],
+                  ),
+
+                  Row(
+                    children: [
+                      Radio<String>(
+                        value: 'Correio',
+                        groupValue: _deliveryType,
+                        onChanged: (v) => setState(() => _deliveryType = v!),
+                      ),
+
+                      const Text("Correio"),
+                    ],
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 5),
+
+              const Category(
+                child: Text(
+                  'Escolha a região:',
+                  style: TextStyle(color: Colors.black87),
+                ),
+              ),
+
+              Center(
+                child: SizedBox(
+                  width: 200,
+                  child: DropdownButtonFormField<String>(
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    iconSize: 24,
+                    hint: const Text("Selecione"),
+                    isExpanded: true,
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 8,
+                      ),
+                    ),
+
+                    items: ['Norte', 'Sul', 'Leste', 'Oeste', 'Centro']
+                        .map(
+                          (String value) => DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedRegion = newValue;
+                      });
+                    },
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 5),
+
+              const Category(
+                child: Text(
+                  'Deseja receber promoções via e-mail?',
+                  style: TextStyle(color: Colors.black87),
+                ),
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Checkbox(
+                    value: _acceptEmail,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _acceptEmail = value ?? false;
+                      });
+                    },
+                  ),
+
+                  const Flexible(
+                    child: Text("Sim, eu desejo receber promoções via e-mail."),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 5),
+
+              Center(
+                child: SizedBox(
+                  width: 150,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 90, 93, 95),
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _resultProduct = _product;
+                        _resultQuantity = _quantity;
+                        _resultDeliveryType = _deliveryType;
+                        _resultRegion = _selectedRegion;
+                        _resultAcceptEmail = _acceptEmail;
+                        _controller.clear();
+                      });
+                    },
+                    child: const Text('Cadastrar'),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 5),
+
+              if (_resultProduct != null)
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'O produto selecionado foi: $_resultProduct, '
+                    'mas a quantidade de produtos escolhida foi ${_resultQuantity!.toInt()}. '
+                    'O pedido será entregue via $_resultDeliveryType. '
+                    'Já a região de entrega será ${_resultRegion ?? "Não informada"}. '
+                    'O cliente aceita envio de promoções ${_resultAcceptEmail! ? "Aceito" : "Não aceito"}.',
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
